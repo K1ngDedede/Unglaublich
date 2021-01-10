be9352c282eece7910c441cc5145bd834397b9b8
afdaza27 <af.daza@uniandes.edu.co>
2021-01-09 22:28:55 -0500

    helpert

--- tests.py
+++ tests.py
@@ -3,27 +3,23 @@ from settings import *
 from engine import *
 
 
+
+
+
 pg.init()
 screen = pg.display.set_mode((WIDTH, HEIGHT))
 pg.display.set_caption(TITULO)
-doge_map = Map("example.txt", screen)
-doge_map.load()
-knight2 = Nameless(1,1,screen, doge_map, False)
-knight = Nameless(1,1,screen, doge_map, True)
-party = Party([knight, knight2], screen)
-pg.key.set_repeat(1, 500)
 clock = pg.time.Clock()
 pg.mixer.music.load("./music/Prueba_SONYsonido.ogg")
 pg.mixer.music.play(-1)
 
-
+#helpert test
+img_helpert = pg.image.load("imgs/helpert-normaldialog.png").convert_alpha()
+img_helpert = pg.transform.scale(img_helpert,(400,500))
 
 while 1:
     for event in pg.event.get():
         if event.type == pg.QUIT: sys.exit()
     dt = clock.tick(FPS) / 1000
-    party.update()
-
-    party.draw()
+    screen.blit(img_helpert, (700, 350))
     pg.display.flip()
-
